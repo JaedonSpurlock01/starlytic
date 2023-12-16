b4bd28bdcf4972c1b93001ea2d474ee47ba2b87a
JaedonSpurlock01 <jaedonaspurlock@outlook.com>
2023-12-15 18:53:23 -0800

    layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className}`}>{children}</body>
+      <body className={`${inter.className} h-full w-full bg-stone-900`}>
+        {children}
+      </body>
     </html>
   );
 }
